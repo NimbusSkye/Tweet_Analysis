409182281d1207f30648aca1db707bd52b2f86a8
Ubuntu <ubuntu@ip-172-31-24-179.us-east-2.compute.internal>
2021-12-07 21:55:28 +0000

    Code for analysis of relative categories/sentiment within categories

---
+++ relative_categories.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import json
+import pprint
+import sys
+
+def main():
+    if len(sys.argv)!=2:
+        print('Invalid # arguments. Exiting.')
+        quit()
+
+    annotations = sys.argv[1]
+
+   # annotations = "annotated_800_hamza.csv"
+    anno_df_1 = pd.read_csv(annotations)
+
+    tags = ["mv", "gv", "pv", "pc", "cd", "c", "h", "cs"]
+    sent = ["p","n","x"]
+    dict ={}
+
+    for t in tags:
+        total = (len(anno_df_1))
+        dict[t]= (len(anno_df_1[anno_df_1["coding"] == t]))/total
+
+    pprint.pprint(dict)
+
+    with open('relative_categories.json','w') as f:
+        json.dump(dict,f)
+
+
+if __name__ == "__main__":
+        main()
---
+++ relative_sentiment.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import json
+import pprint
+import sys
+
+def main():
+    if len(sys.argv)!=2:
+        print('Invalid # arguments. Exiting.')
+        quit()
+
+    annotations = sys.argv[1]
+
+   # annotations = "annotated_800_hamza.csv"
+    anno_df_1 = pd.read_csv(annotations)
+
+    tags = ["mv", "gv", "pv", "pc", "cd", "c", "h", "cs"]
+    sent = ["p","n","x"]
+    dict ={}
+
+    for t in tags:
+        total = (len(anno_df_1[anno_df_1["coding"] == t]))
+        dict[t]={}
+
+        for s in sent:
+            dict[t][s]=(len(anno_df_1[(anno_df_1["coding"] == t) & (anno_df_1["sentiment"] == s)]))/total
+            
+    pprint.pprint(dict)
+
+    with open('relative_sentiment.json','w') as f:
+        json.dump(dict,f)
+
+
+if __name__ == "__main__":
+        main()
